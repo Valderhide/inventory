9f713cc031b23386f3d815a4b072340ffad59212
Valderhide <gpinsent@hotmail.com>
2021-10-20 18:11:51 -0230

    Breadcrumb Test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button, Alert,} from 'react-bootstrap';
+import { Button, Alert, Breadcrumb, BreadcrumbItem } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
 
@@ -10,6 +10,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Breadcrumb>
+          <BreadcrumbItem active>TEST</BreadcrumbItem>
+          <BreadcrumbItem>TEST2</BreadcrumbItem>
+          <BreadcrumbItem active>TEST3</BreadcrumbItem>
+          <BreadcrumbItem>TEST4</BreadcrumbItem>
+          <BreadcrumbItem active>TEST5</BreadcrumbItem>
+        </Breadcrumb>
         <Alert variant="success"> This is an Alert</Alert>
         <Button>Test Button</Button>
         <p>
