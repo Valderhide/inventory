9e1ac70b29831ac132e743899121be0f64bca3c3
Valderhide <gpinsent@hotmail.com>
2021-10-20 19:33:54 -0230

    FInished Round 1 of React/Bootstrap testing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button, Alert, Breadcrumb, BreadcrumbItem, Card, Form } from 'react-bootstrap';
+import { Container, Row, Col, Button, Alert, Breadcrumb, BreadcrumbItem, Card, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
 
@@ -9,30 +9,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        <Form>
-          <Form.Group>
-            <Form.Label>Email Adress</Form.Label>
-            <Form.Control type="email" placeholder="Example@email.com"/>
-            <Form.Text classname="text-muted">
-              Lorem ipsum dolor sit amet, consectetur.
-            </Form.Text>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="PASSWORD"/>
-          </Form.Group>
-          <Button variant="secondary" type="submit">Login</Button>
-        </Form>
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <Card classname="mb-3" style={{ color: "black" }}>
-          <Card.Img src="https://picsum.photos/200/100" />
-          <Card.Body>
-            <Card.Title> Title Card Example </Card.Title>
-            <Card.Text> This is an example of Card.text </Card.Text>
-            <Button variant="primary">Read More</Button>
-          </Card.Body>
-        </Card>
         <Breadcrumb>
           <BreadcrumbItem active>Fabrics</BreadcrumbItem>
           <BreadcrumbItem>Tools</BreadcrumbItem>
@@ -40,8 +16,38 @@ function App() {
           <BreadcrumbItem>Consumables</BreadcrumbItem>
           <BreadcrumbItem active>Misc</BreadcrumbItem>
         </Breadcrumb>
-        <Alert variant="success"> This is an Alert</Alert>
-        <Button>Test Button</Button>
+        <Container>
+          <Form>
+            <Row>
+              <Col md>
+                <Form.Group controlId="formEmail">
+                  <Form.Label>Email Adress</Form.Label>
+                  <Form.Control type="email" placeholder="Example@email.com" />
+                  <Form.Text classname="text-muted">
+                    Lorem ipsum dolor sit amet, consectetur.
+                  </Form.Text>
+                </Form.Group>
+              </Col>
+              <Col md>
+                <Form.Group controlId="formPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="PASSWORD" />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Button variant="secondary" type="submit">Login</Button>
+          </Form>
+          <img src={logo} className="App-logo" alt="logo" />
+          <Card classname="mb-3" style={{ color: "black" }}>
+            <Card.Body>
+              <Card.Title> Title Card Example </Card.Title>
+              <Card.Text> This is an example of Card.text </Card.Text>
+              <Button variant="primary">Read More</Button>
+            </Card.Body>
+          </Card>
+          <Alert variant="success"> This is an Alert</Alert>
+          <Button>Test Button</Button>
+        </Container>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
