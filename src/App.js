6ef564ae3d7afdbfebae793f675fe6d59ab3001d
Valderhide <gpinsent@hotmail.com>
2021-10-20 19:09:20 -0230

    Form/log in testing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button, Alert, Breadcrumb, BreadcrumbItem, Card } from 'react-bootstrap';
+import { Button, Alert, Breadcrumb, BreadcrumbItem, Card, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
 
@@ -9,21 +9,36 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Card classname="mb-3" style={{color: "black"}}>
-          <Card.Img src="https://picsum.photos/200/100"/>
+        
+        <Form>
+          <Form.Group>
+            <Form.Label>Email Adress</Form.Label>
+            <Form.Control type="email" placeholder="Example@email.com"/>
+            <Form.Text classname="text-muted">
+              Lorem ipsum dolor sit amet, consectetur.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="PASSWORD"/>
+          </Form.Group>
+          <Button variant="secondary" type="submit">Login</Button>
+        </Form>
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <Card classname="mb-3" style={{ color: "black" }}>
+          <Card.Img src="https://picsum.photos/200/100" />
           <Card.Body>
             <Card.Title> Title Card Example </Card.Title>
             <Card.Text> This is an example of Card.text </Card.Text>
             <Button variant="primary">Read More</Button>
           </Card.Body>
         </Card>
-        <img src={logo} className="App-logo" alt="logo" />
         <Breadcrumb>
-          <BreadcrumbItem active>TEST</BreadcrumbItem>
-          <BreadcrumbItem>TEST2</BreadcrumbItem>
-          <BreadcrumbItem active>TEST3</BreadcrumbItem>
-          <BreadcrumbItem>TEST4</BreadcrumbItem>
-          <BreadcrumbItem active>TEST5</BreadcrumbItem>
+          <BreadcrumbItem active>Fabrics</BreadcrumbItem>
+          <BreadcrumbItem>Tools</BreadcrumbItem>
+          <BreadcrumbItem active>Threads</BreadcrumbItem>
+          <BreadcrumbItem>Consumables</BreadcrumbItem>
+          <BreadcrumbItem active>Misc</BreadcrumbItem>
         </Breadcrumb>
         <Alert variant="success"> This is an Alert</Alert>
         <Button>Test Button</Button>
