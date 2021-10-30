a478bbee12c07eb4762de9a0226dcc33e53d3442
Valderhide <gpinsent@hotmail.com>
2021-10-30 12:58:36 -0230

    Ui Design 0.2.1: Additional Forms and form arrangment

--- src/App.js
+++ src/App.js
@@ -33,30 +33,46 @@ function App() {
             )}
           </>
           <Form>
-          <Form.Group controlId="formName">
-              <Form.Label>Name</Form.Label>
-              <Form.Control placeholder="Name of Material" />
+            <Form.Group controlId="formFile" className="mb-3">
+              <Form.Label>Default file input example</Form.Label>
+              <Form.Control type="file" />
             </Form.Group>
-            <Form.Group controlId="formColor">
-              <Form.Label>Color</Form.Label>
-              <Form.Control placeholder="Color" />
-            </Form.Group>
-            <Form.Group controlId="formLength">
-              <Form.Label>Length</Form.Label>
-              <Form.Control placeholder="Length" />
-            </Form.Group>
-            <Form.Group controlId="formPrice">
-              <Form.Label>Price</Form.Label>
-              <Form.Control placeholder="Price" />
+            <Row>
+              <Col md>
+                <Form.Group controlId="formName">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control placeholder="Name of Material" />
+                </Form.Group>
+              </Col>
+              <Col md>
+                <Form.Group controlId="formColor">
+                  <Form.Label>Color</Form.Label>
+                  <Form.Control placeholder="Color" />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col md>
+                <Form.Group controlId="formLength">
+                  <Form.Label>Amount</Form.Label>
+                  <Form.Control placeholder="Length" />
+                </Form.Group>
+              </Col>
+              <Col md>
+                <Form.Group controlId="formPrice">
+                  <Form.Label>Price</Form.Label>
+                  <Form.Control placeholder="Price" />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Form.Group controlId="formDescription">
+              <Form.Label>Item Description</Form.Label>
+              <Form.Control placeholder="Item Description" />
             </Form.Group>
             <Form.Group controlId="formLocation">
               <Form.Label>Store Location</Form.Label>
               <Form.Control placeholder="Store Location" />
             </Form.Group>
-            
-            
-            
-            
             <Button variant="secondary" type="submit">Submit</Button>
           </Form>
         </Container>
