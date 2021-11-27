87faffe3ea7af18395a234d6e60ef1d4cc75e1a6
Valderhide <gpinsent@hotmail.com>
2021-11-27 16:12:52 -0330

    unknown handleSubmit issue

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddItemForm from './ItemForm.js';
 import ItemTable from './Table.js';
+import categories from './categories.json';
 <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
 
 
 
 
 function App() {
+  
   const handleSubmit = (e) => {
-    const picture = e.target[0].value;
-    const name = e.target[1].value;
-    const color = e.target[2].value;
-    const amount = e.target[3].value;
-    const price = e.target[4].value;
-    const description = e.target[5].value;
-    const store = e.target[6].value;
+    const offset = categories.length;
+    const picture = e.target[offset].value;
+    const name = e.target[offset + 1].value;
+    const color = e.target[offset + 2].value;
+    const amount = e.target[offset + 3].value;
+    const price = e.target[offset + 4].value;
+    const description = e.target[offset + 5].value;
+    const store = e.target[offset + 6].value;
     //localStorage.setItem({ picture, name, color, amount, price, description, store });
     console.log({ picture, name, color, amount, price, description, store });
+    console.log(e.target[offset])
     // Put console log for final result here...
     e.preventDefault();
   }
