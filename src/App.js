8f76c3dba0ef1ea51aac8d05121252730324a4d9
Valderhide <gpinsent@hotmail.com>
2021-12-10 20:38:45 -0330

    Refactored localStorage setItem

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import AddItemForm from './ItemForm.js';
 import ItemTable from './Table.js';
 import categories from './categories.json';
-<script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
 
 
 
@@ -12,16 +11,20 @@ function App() {
 
   const handleSubmit = (e) => {
     const offset = categories.length;
-    const picture = e.target[offset +9].value;
+    const picture = e.target[offset + 9].value;
     const name = e.target[offset + 10].value;
     const color = e.target[offset + 11].value;
     const amount = e.target[offset + 12].value;
     const price = e.target[offset + 13].value;
     const description = e.target[offset + 14].value;
     const store = e.target[offset + 15].value;
-    localStorage.clear();
+    //localStorage.clear();
     console.log({ picture, name, color, amount, price, description, store });
-    localStorage.setItem('Data', JSON.stringify({a: picture, b: name, c: color, d: amount, e: price, f: description, g: store}));
+    let originalValue = localStorage.getItem('properties');
+    let previousValue = JSON.parse(originalValue) ?? [];
+    let newValue = { picture, name, color, amount, price, description, store };
+    previousValue.push(newValue);
+    localStorage.setItem('properties', JSON.stringify(previousValue));
     // Put console log for final result here...
     e.preventDefault();
   }
