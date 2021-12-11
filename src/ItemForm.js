8c6b9a588590c2e4202a28e356bdd02c69927e9a
Valderhide <gpinsent@hotmail.com>
2021-12-11 13:56:49 -0330

    Categories container styling attempt 1

--- src/ItemForm.js
+++ src/ItemForm.js
@@ -14,7 +14,7 @@ function Category({ title, items, selectedCategory, setSelectedCategory }) {
   return (
     <Col>
       <Accordion>
-        <Accordion.Item eventKey="0">
+        <Accordion.Item eventKey="0" classname ="categories-container">
           <Accordion.Header>{title}</Accordion.Header>
           {items.map(({ titles, subCategories }) => (
             <Accordion.Body>
@@ -24,6 +24,7 @@ function Category({ title, items, selectedCategory, setSelectedCategory }) {
                 variant="primary"
                 onSelect={handleSelect}
                 title={titles}
+                
               >
                 {subCategories.map(subCategory => (
                   <Dropdown.Item key={subCategory} eventKey={subCategory}>{subCategory}</Dropdown.Item>
@@ -42,54 +43,54 @@ export default function AddItemForm({ handleSubmit }) {
   const [selectedCategory, setSelectedCategory] = useState({});
   return (
     <>
-    {categories.map(
-      ({ title, items }) => (
-        <Category key={title} title={title} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} items={items} />
-      ),
-    )}
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="formFile" className="mb-3">
-        <Form.Label>Item Picture</Form.Label>
-        <Form.Control type="file" />
-      </Form.Group>
-      <Row>
-        <Col md>
-          <Form.Group controlId="formName">
-            <Form.Label>Name</Form.Label>
-            <Form.Control placeholder="Name of Material" />
-          </Form.Group>
-        </Col>
-        <Col md>
-          <Form.Group controlId="formColor">
-            <Form.Label>Color</Form.Label>
-            <Form.Control placeholder="Color" />
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col md>
-          <Form.Group controlId="formLength">
-            <Form.Label>Amount</Form.Label>
-            <Form.Control placeholder="Length" />
-          </Form.Group>
-        </Col>
-        <Col md>
-          <Form.Group controlId="formPrice">
-            <Form.Label>Price</Form.Label>
-            <Form.Control placeholder="Price" />
-          </Form.Group>
-        </Col>
-      </Row>
-      <Form.Group controlId="formDescription">
-        <Form.Label>Item Description</Form.Label>
-        <Form.Control placeholder="Item Description" style={{ height: '100px' }} />
-      </Form.Group>
-      <Form.Group controlId="formLocation">
-        <Form.Label>Store Location</Form.Label>
-        <Form.Control placeholder="Store Location" />
-      </Form.Group>
-      <Button variant="secondary" type="submit">Submit</Button>
-    </Form>
+      {categories.map(
+        ({ title, items }) => (
+          <Category key={title} title={title} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} items={items} />
+        ),
+      )}
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formFile" className="mb-3">
+          <Form.Label>Item Picture</Form.Label>
+          <Form.Control type="file" />
+        </Form.Group>
+        <Row>
+          <Col md>
+            <Form.Group controlId="formName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control placeholder="Name of Material" />
+            </Form.Group>
+          </Col>
+          <Col md>
+            <Form.Group controlId="formColor">
+              <Form.Label>Color</Form.Label>
+              <Form.Control placeholder="Color" />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col md>
+            <Form.Group controlId="formLength">
+              <Form.Label>Amount</Form.Label>
+              <Form.Control placeholder="Length" />
+            </Form.Group>
+          </Col>
+          <Col md>
+            <Form.Group controlId="formPrice">
+              <Form.Label>Price</Form.Label>
+              <Form.Control placeholder="Price" />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Form.Group controlId="formDescription">
+          <Form.Label>Item Description</Form.Label>
+          <Form.Control placeholder="Item Description" style={{ height: '100px' }} />
+        </Form.Group>
+        <Form.Group controlId="formLocation">
+          <Form.Label>Store Location</Form.Label>
+          <Form.Control placeholder="Store Location" />
+        </Form.Group>
+        <Button variant="secondary" type="submit">Submit</Button>
+      </Form>
     </>
   );
 }
