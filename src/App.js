e209c91e0170009dca47ce2ea17422f2a795e2cc
Valderhide <gpinsent@hotmail.com>
2021-12-17 16:48:34 -0330

    Basic Table reimplemented

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function App() {
                 </Button>
               </Modal.Footer>
             </Modal>
-            {/*<ItemTable />*/}
+            <ItemTable />
           </header>
         </div>
       </>
