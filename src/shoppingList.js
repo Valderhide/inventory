203e9fccfdefde804db21f897ec57859b5b7fdb4
Valderhide <gpinsent@hotmail.com>
2022-03-25 22:33:49 -0230

    Printer friendly table made in shoppingList.js

--- src/shoppingList.js
+++ src/shoppingList.js
@@ -1,10 +1,33 @@
-let data = JSON.parse(localStorage.getItem("properties"));
-
-let sum = data.reduce(
-  (accumulator, data) => accumulator + parseInt(data.price),
-  0
-);
+import { Table } from "react-bootstrap";
 
 export default function Print() {
-  return <main> {(window.onload = sum)} </main>;
+  let data = JSON.parse(localStorage.getItem("properties"));
+
+  let sum = data.reduce(
+    (accumulator, data) => accumulator + parseInt(data.price),
+    0
+  );
+
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map(({ name, price }) => (
+          <tr>
+            <td>{name}</td>
+            <td>{price}</td>
+          </tr>
+        ))}
+      </tbody>
+      <tfoot>
+        <td> Total</td>
+        <td> {(window.onload = sum)} </td>
+      </tfoot>
+    </Table>
+  );
 }
