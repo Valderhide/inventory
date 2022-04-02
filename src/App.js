06f3169f9846ea726b76bc3d6f56a7a351dc57fb
Valderhide <gpinsent@hotmail.com>
2022-04-01 23:38:07 -0230

    Basic React Bootstrap Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { Link, Outlet } from "react-router-dom";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 export default function App() {
   return (
     <div>
-      <nav className="home">
-        <h1 clas>Inventory System Home Page</h1>
-        <Link to="/">HomePage</Link> | <Link to="/inventory">Inventory</Link>
-      </nav>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/inventory">Inventory</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <Outlet />
     </div>
   );
