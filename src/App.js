9f5b56ee48e327fd9ac92de40046e1d239605417
Valderhide <gpinsent@hotmail.com>
2022-04-03 13:55:25 -0230

    Minor updates and edit to Navbar

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 export default function App() {
   return (
     <div>
-      <Navbar bg="light" expand="lg">
+      <Navbar>
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Brand href="/">Quilting Inventory</Navbar.Brand>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/inventory">Inventory</Nav.Link>
+              <Nav.Link href="/inventory">Database</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
