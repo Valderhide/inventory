f465d40fc7955658c8ed47de45fbc62cefbf2f07
Valderhide <gpinsent@hotmail.com>
2022-04-04 23:34:01 -0230

    fixed file structure names

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ export default function App() {
           <Navbar.Brand href="/">Quilting Inventory</Navbar.Brand>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
+              <Nav.Link href="/Homepage">Homepage</Nav.Link>
               <Nav.Link href="/inventory">Database</Nav.Link>
             </Nav>
           </Navbar.Collapse>
---
+++ src/Pages/homepage.js
@@ -0,0 +1,10 @@
+export default function HomePage() {
+  return (
+    <div className="descript">
+      Enim magna sunt laborum deserunt. Magna minim ipsum laborum velit anim
+      occaecat irure culpa exercitation officia esse sunt. Amet cillum voluptate
+      excepteur et cupidatat laboris irure do adipisicing pariatur laborum.
+      Consequat anim elit officia deserunt commodo aliquip qui irure esse.
+    </div>
+  );
+}
