888a427d6900138f3cbd01fe77c5187f8998dfbf
Valderhide <gpinsent@hotmail.com>
2022-05-16 17:31:36 -0230

    clean slate for moving filters to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { render } from "react-dom";
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
@@ -9,29 +8,15 @@ import HomePage from "./pages/Homepage";
 
 const rootElement = document.getElementById("root");
 
-// filter1(state, price), filter2(state, amount), filter3(state), filter4(state)
-// filter(state) = {price: 10, amount: 20}
-
-const InventoryApp = () => {
-  const [tableData, setTableData] = useState();
-  const [filters, setFilters] = useState();
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<HomePage />} />
-          <Route
-            path="inventory"
-            element={<Inv tableData={tableData} setTableData={setTableData} />}
-          />
-        </Route>
-        <Route
-          path="shoppingList"
-          element={<Print tableData={tableData} setTableData={setTableData} />}
-        />
-      </Routes>
-    </BrowserRouter>
-  );
-};
-
-render(<InventoryApp />, rootElement);
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="inventory" element={<Inv />} />
+      </Route>
+      <Route path="shoppingList" element={<Print />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+);
