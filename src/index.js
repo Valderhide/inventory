ef654b3314a9671133548111356dadc9b93aebcc
Valderhide <gpinsent@hotmail.com>
2022-05-17 20:03:00 -0230

    removed Homepage.js to attempt pathspec error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import "./index.css";
 import App from "./App";
 import Inv from "./pages/Inventory";
@@ -8,15 +9,54 @@ import HomePage from "./pages/Homepage";
 
 const rootElement = document.getElementById("root");
 
-render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<HomePage />} />
-        <Route path="inventory" element={<Inv />} />
-      </Route>
-      <Route path="shoppingList" element={<Print />} />
-    </Routes>
-  </BrowserRouter>,
-  rootElement
-);
+// filter1(state, price), filter2(state, amount), filter3(state), filter4(state)
+// filter(state) = {price: 10, amount: 20}
+
+const InventoryApp = () => {
+  const [filter1, setFilter1] = useState("");
+  const [filter2, setFilter2] = useState("");
+  const [filter3, setFilter3] = useState("");
+  const [filter4, setFilter4] = useState("");
+  const [filter5, setFilter5] = useState("");
+  const [filter6, setFilter6] = useState("");
+  const [filter7, setFilter7] = useState("");
+  let filterObj = {
+    filter1,
+    filter2,
+    filter3,
+    filter4,
+    filter5,
+    filter6,
+    filter7,
+  };
+  let setFilterObj = {
+    setFilter1,
+    setFilter2,
+    setFilter3,
+    setFilter4,
+    setFilter5,
+    setFilter6,
+    setFilter7,
+  };
+  const [tableData, setTableData] = useState();
+  const [filters, setFilters] = useState();
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route
+            path="inventory"
+            element={<Inv tableData={tableData} setTableData={setTableData} />}
+          />
+        </Route>
+        <Route
+          path="shoppingList"
+          element={<Print tableData={tableData} setTableData={setTableData} />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+render(<InventoryApp />, rootElement);
